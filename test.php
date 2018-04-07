070495817bb224c5dbdb2b81f4565a0c2e971651
5ganes <kh6ganesh@gmail.com>
2018-04-07 20:10:33 +0545

    some changes

--- test.php
+++ test.php
@@ -1,27 +1,11 @@
 <?php
-mysql_connect('localhost', 'root', '');
-mysql_select_db('mrsmp');
-mysql_set_charset("utf8");
-$rows = mysql_query('SELECT * FROM tbl_crop');
-
-//Store it into the array
-while ($row = mysql_fetch_array($rows)){
-	$cName[] =$row["cropName"];
-	$fYear[] =$row["fiscalYear"];
-};
-//Write it to CSV file
-$output = fopen("php://output",'w') or die("Can't open php://output");
-header("Content-Type:application/csv"); 
-header("Content-Disposition:attachment;filename=data.csv");
-echo "\xEF\xBB\xBF"; //Important use UTF8 BOM to convert special characters
-fputcsv($output, array('Fiscal Year','Crop Name'));
-for($c=0;$c<strlen($cName);$c++)
-{
-	fputcsv($output, array($fYear[$c], $cName[$c]));
-	//fputcsv($output,explode(',',$fiscalYear));
-	//fputcsv($output,explode(',',$cropName));	
-}
-
-fclose($output) or die("Can't close php://output");
-
+	ini_set('display_errors', 1);
+	$host = "103.21.59.7";
+    $uname = "aiccgov_ictan"; 		
+    $psw = "K0G9wM3Shz,a";					
+	$dbname = "aiccgov_ictan";
+	
+	$links = mysql_connect($host, $uname, $psw) or die("Sorry, couldnot connect to MySQL Server");
+	$db = mysql_select_db($dbname,$links) or die("Sorry, couldnot find database");	
+	echo 'Hello World';
 ?>
